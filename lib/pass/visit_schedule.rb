e133cbadeffe91849165db50af38d4aa4963f25d
Omar Qureshi <omar@omarqureshi.net>
2025-05-01 11:37:24 +0100

    Add API based filtering to VisitSchedule

--- lib/pass/visit_schedule.rb
+++ lib/pass/visit_schedule.rb
@@ -11,6 +11,19 @@ module PASS
     end
 
     class << self
+      def list(filters: {})
+        response = PASS::Client.instance.connection.get list_endpoint do |request|
+          # TODO: Remove this
+          request.params["page[size]"] = 10000000
+          active_query_filters(filters).each do |k, v|
+            request.params["filter[#{k}]"] = v
+          end
+        end
+        collection = extract_list_from_response(response)
+        filter_collection(filters, collection)
+      end
+
+
       def list_endpoint
         "visit-schedules"
       end
@@ -19,6 +32,10 @@ module PASS
         {
           :study_id => OpenStruct.new(type: :studies, label: :study)
         }
+      end
+
+      def query_filters
+        %w(study)
       end
     end
 
