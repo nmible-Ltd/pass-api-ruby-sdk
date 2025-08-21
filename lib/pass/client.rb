007577e263bed982184f79773b9e061e667c30ec
EmanuelH <emanuel.hafner@nmible.com>
2025-08-21 16:05:21 +0100

    Updated response body for pass client login request

--- lib/pass/client.rb
+++ lib/pass/client.rb
@@ -27,14 +27,14 @@ module PASS
                                                  }}
 
 
-      @access_token = login_response.body[:data][:attributes][:access]
-      @refresh_token = login_response.body[:data][:attributes][:refresh]
+      @access_token = login_response.body[:data][:meta][:token]
+      # @refresh_token = login_response.body[:data][:attributes][:refresh]
 
       # TODO: implement refresh behaviour - for now this is ok - the expiry time is 24 hours
       # Although in this case the refresh token expires in 48 hours
 
       @access_token_expiry = Time.at(JWT.decode(@access_token, nil, false)[0]["exp"])
-      @refresh_token_expiry = Time.at(JWT.decode(@refresh_token, nil, false)[0]["exp"])
+      # @refresh_token_expiry = Time.at(JWT.decode(@refresh_token, nil, false)[0]["exp"])
     end
 
     def authenticated?
